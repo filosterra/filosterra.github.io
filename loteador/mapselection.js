d3cbcc13d8f30fc85bb0ca685c23f1256da909fa
Uriel Gomez <uriel@panax.io>
2024-03-20 13:56:32 -0600

    - 20240320_1347 - * Ajustes en filtros en loteador * Ajustes de estilos en loteador * Agreganndo característica de zoom para loteadores

--- loteador/mapselection.js
+++ loteador/mapselection.js
@@ -179,8 +179,8 @@ async function actualizaColores(oSource) {
 function coloreaLote(oLote, color = '') {
     let data = $(oLote).data('maphilight') || {};
     //data.alwaysOn = false;
-    data.fillColor = color && color.replace(/^0x|^#/ig, "") || "ffffff";
-    data.fillOpacity = color ? 0.5 : 0.1;
+    data.fillColor = color && color.replace(/^0x|^#/ig, "") || "000000";
+    data.fillOpacity = color ? 0.5 : 0.2;
     data.strokeColor = "ffffff";
     $(this).data('maphilight', data).trigger('alwaysOn.maphilight');
     $(this).data('maphilight', data).trigger('fillColor.maphilight');
